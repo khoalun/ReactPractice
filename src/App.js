740dbe3e116d1a3c2dab6b69052e2fa97bc5b9fa
khoalun <buikhoa793@gmail.com>
2020-08-03 14:57:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TodoItem from './components/TodoItem';
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = [
+      'Dit gai',
+      'Chich gai',
+      'Phich gai'
+    ];
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        {
+          this.todoItems.map((item, index) => <TodoItem key={index} title={item} />)
+        }
+      </div>
+    );
+  }
 }
+
 
 export default App;
